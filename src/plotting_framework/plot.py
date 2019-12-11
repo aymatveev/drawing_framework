a9bda33e07decdc59c77e28e6fc0081f4add6978
aymatveev <aymatveev@itmo.ru>
2019-12-11 07:08:47 +0300

    working init

--- src/plotting_framework/plot.py
+++ src/plotting_framework/plot.py
@@ -1,4 +1,5 @@
 from typing import Iterable, Tuple
+import colors
 
 def remove_linebreaks(s: str) -> str:
     return "".join(s.split("\n"))
@@ -9,13 +10,12 @@ def shift_data(data: Iterable[Tuple[float, float]], offset: Tuple[float, float])
 def scale_data(data: Iterable[Tuple[float, float]], scale: Tuple[float, float]) -> Iterable[Tuple[float, float]]:
     return ((p[0] * scale[0], p[1] * scale[1]) for p in data)
 
-def mark_point(point: Tuple[int, int]) -> str:
-    return f"""<circle cx="{point[0]}" cy="{point[1]}" r="3"/>"""
-
-def scatter(data: Iterable[Tuple[float, float]], bl: Tuple[float, float], tr: Tuple[float, float]) -> str:
-    width = 640
-    height = 480
-
+def transform_to_screen_plane(
+    data: Iterable[Tuple[float, float]],
+    bl: Tuple[float, float],
+    tr: Tuple[float, float],
+    width: int = 640,
+    height: int = 480) -> Iterable[Tuple[float, float]]:
     middle = ((bl[0] + tr[0]) / 2, (bl[1] + tr[1]) / 2)
     viewport_middle = (- width / 2, - height / 2)
 
@@ -27,14 +27,44 @@ def scatter(data: Iterable[Tuple[float, float]], bl: Tuple[float, float], tr: Tu
     scaled_data_int: Iterable[Tuple[int, int]] = ((round(p[0]), round(p[1])) for p in shifted_data)
     flipped_y_data: Iterable[Tuple[int, int]] = ((p[0], height - p[1]) for p in scaled_data_int)
 
-    result = remove_linebreaks(f"""
-    <svg width="{width}" height="{height}">
+    return flipped_y_data
+
+def svg_wrap(width: int, height: int, content: str) -> str:
+    return f"""<svg width="{width}" height="{height}">{content}</svg>"""
+
+def draw_axes(width: int, height: int, bl: Tuple[float, float], tr: Tuple[float, float]) -> str:
+    return remove_linebreaks(f"""
         <line x1="0" y1="{height}" x2="{width}" y2="{height}" stroke="black" />
         <line x1="0" y1="0" x2="0" y2="{height}" stroke="black" />
         <text x="10" y="{height - 10}" class="small">{bl}</text>
         <text x="{width - 80}" y="{15}" class="small">{tr}</text>
-        { "".join(mark_point(p) for p in flipped_y_data) }
-    </svg>
     """)
 
-    return result
+def mark_point(point: Tuple[int, int], size: int = 3, color: str = "black") -> str:
+    return f"""<circle cx="{point[0]}" cy="{point[1]}" r="{size}" style="fill: {color}"/>"""
+
+def join_series_of_points(data: Iterable[Tuple[float, float]], point_size: int = 3, color: str = "black") -> str:
+    return "".join(mark_point(p, point_size, color) for p in data)
+
+def scatter(
+    data: Iterable[Iterable[Tuple[float, float]]],
+    bl: Tuple[float, float],
+    tr: Tuple[float, float],
+    point_size: int = 3,
+    width: int = 640,
+    height: int = 480) -> str:
+    color_gen = colors.color_generator()
+    transformed_data = tuple(transform_to_screen_plane(series, bl, tr, width, height) for series in data)
+    return svg_wrap(width, height, remove_linebreaks(f"""
+        {draw_axes(width, height, bl, tr)}
+        {"".join(join_series_of_points(series, point_size, next(color_gen)) for series in transformed_data)}
+    """))
+
+def scatter_xy(
+    data: Iterable[Tuple[float, float]],
+    bl: Tuple[float, float],
+    tr: Tuple[float, float],
+    point_size: int = 3,
+    width: int = 640,
+    height: int = 480) -> str:
+    return scatter((data,), bl, tr, point_size, width, height)
--- src/plotting_framework/plot_test.py
+++ src/plotting_framework/plot_test.py
@@ -1,12 +1,16 @@
 from typing import Iterable, Tuple
+#from . import plot
 import plot
 import importlib
 
-def test_plot1():
+def test_plot3():
     importlib.reload(plot)
 
-    data: Iterable[Tuple[int, int]] = ((0, 0), (10, 10))
-    print(f"Data: {data}")
+    data: Iterable[Iterable[Tuple[int, int]]] = (
+        ((0, 0), (-10, -10)),
+        ((10, 10), (-9, -9)),
+        ((5, 5), (-6, 8))
+    )
     result = plot.scatter(data, (-20, -20), (20, 20))
     print(result)
 
@@ -14,5 +18,13 @@ def test_plot2():
     importlib.reload(plot)
 
     data: Iterable[Tuple[int, int]] = ((0, 0), (-10, -10))
-    result = plot.scatter(data, (-20, -20), (20, 20))
+    result = plot.scatter_xy(data, (-20, -20), (20, 20))
+    print(result)
+
+def test_plot1():
+    importlib.reload(plot)
+
+    data: Iterable[Tuple[int, int]] = ((0, 0), (10, 10))
+    print(f"Data: {data}")
+    result = plot.scatter_xy(data, (-20, -20), (20, 20))
     print(result)
