8bfeac56840a8fd5fb5cabb1df21524046a38b24
aymatveev <aymatveev@itmo.ru>
2019-12-12 04:16:09 +0300

    animated plot

--- src/plotting_framework/plot.py
+++ src/plotting_framework/plot.py
@@ -27,7 +27,7 @@ def transform_to_screen_plane(
     scaled_data_int: Iterable[Tuple[int, int]] = ((round(p[0]), round(p[1])) for p in shifted_data)
     flipped_y_data: Iterable[Tuple[int, int]] = ((p[0], height - p[1]) for p in scaled_data_int)
 
-    return flipped_y_data
+    return tuple(flipped_y_data)
 
 def svg_wrap(width: int, height: int, content: str) -> str:
     return f"""<svg width="{width}" height="{height}">{content}</svg>"""
@@ -45,6 +45,84 @@ def mark_point(point: Tuple[int, int], size: int = 3, color: str = "black") -> s
 
 def join_series_of_points(data: Iterable[Tuple[float, float]], point_size: int = 3, color: str = "black") -> str:
     return "".join(mark_point(p, point_size, color) for p in data)
+
+def join_serieses_of_points(data: Iterable[Iterable[Tuple[float, float]]], color_gen: Iterable[str], point_size: int = 3) -> str:
+    return "".join(join_series_of_points(series, point_size, next(color_gen)) for series in data)
+
+def draw_internal(
+    desc: str,
+    data: Iterable[Tuple[float, float]],
+    bl: Tuple[float, float],
+    tr: Tuple[float, float],
+    point_size: int = 3,
+    color: str = "black") -> str:
+    if desc == "points":
+        return join_series_of_points(data, point_size, color)
+    if desc == "vector":
+        p1, p2 = tuple(data)[:2]
+        return f"""<line x1="{p1[0]}" y1="{p1[1]}" x2="{p2[0]}" y2="{p2[1]}" stroke="{color}"/>"""
+    
+    if len(data) != 2:
+        return ""
+    p1, p2 = tuple(data)[:2]
+    k = (p2[1] - p1[1]) / (p2[0] - p1[0])
+    b = p1[1] - k * p1[0]
+    left = (bl[0], k * bl[0] + b)
+    right = (tr[0], k * tr[0] + b)
+
+    return f"""<line x1="{left[0]}" y1="{left[1]}" x2="{right[0]}" y2="{right[1]}" style="fill: {color}"/>"""
+
+
+def draw(
+    data: Iterable[Tuple[str, Iterable[Tuple[float, float]]]],
+    color_gen: Iterable[str],
+    bl: Tuple[float, float],
+    tr: Tuple[float, float],
+    point_size: int = 3) -> str:
+    return "".join(draw_internal(desc, series, bl, tr, point_size, next(color_gen)) for (desc, series) in data)
+
+def get_animation_frame(content: str, duration: int, keytimes: Iterable[str], visible_keytime: int) -> str:
+    values = tuple(("visible" if vidx == visible_keytime else "hidden") for vidx in range(len(keytimes))) + ("hidden",)
+    return remove_linebreaks(f"""
+        <g>
+            {content}
+            <animate attributeName="visibility" dur="{duration}s" repeatCount="indefinite" values="{";".join(values)}" keyTimes="{";".join(tuple(keytimes) + ("1",))}"/>
+        </g>
+    """)
+
+def plot(
+    data: Iterable[Iterable[Tuple[str, Iterable[Tuple[float, float]]]]],
+    bl: Tuple[float, float],
+    tr: Tuple[float, float],
+    animation_duration: int = 10,
+    point_size: int = 3,
+    width: int = 640,
+    height: int = 480) -> str:
+    n_frames = len(data)
+    keytimes = tuple(str(key / n_frames)[:5] for key in range(len(data)))
+    transformed_data = tuple(tuple((desc, transform_to_screen_plane(series, bl, tr, width, height)) for (desc, series) in keyframe) for keyframe in data)
+    transformed_data = tuple(tuple((desc, (series + (series[0],))) for (desc, series) in keyframe) for keyframe in transformed_data)
+    return svg_wrap(width, height, remove_linebreaks(f"""
+        {draw_axes(width, height, bl, tr)}
+        {tuple(get_animation_frame(draw(keyframe, colors.color_generator(), bl, tr, point_size), animation_duration, keytimes, idx) for (idx, keyframe) in enumerate(transformed_data))}
+    """))
+
+def scatter_animate(
+    data: Iterable[Iterable[Iterable[Tuple[float, float]]]],
+    bl: Tuple[float, float],
+    tr: Tuple[float, float],
+    animation_duration: int = 10,
+    point_size: int = 3,
+    width: int = 640,
+    height: int = 480) -> str:
+    n_frames = len(data)
+    keytimes = tuple(str(key / n_frames)[:5] for key in range(len(data)))
+    transformed_data = tuple(tuple(transform_to_screen_plane(series, bl, tr, width, height) for series in keyframe) for keyframe in data)
+    transformed_data = tuple((series + (series[0],) for series in keyframe) for keyframe in transformed_data)
+    return svg_wrap(width, height, remove_linebreaks(f"""
+        {draw_axes(width, height, bl, tr)}
+        {tuple(get_animation_frame(join_serieses_of_points(keyframe, colors.color_generator(), point_size), animation_duration, keytimes, idx) for (idx, keyframe) in enumerate(transformed_data))}
+    """))
 
 def scatter(
     data: Iterable[Iterable[Tuple[float, float]]],
--- src/plotting_framework/plot_test.py
+++ src/plotting_framework/plot_test.py
@@ -3,6 +3,91 @@ from typing import Iterable, Tuple
 import plot
 import importlib
 
+def test_plot8():
+    importlib.reload(plot)
+
+    data: Iterable[Iterable[Iterable[Tuple[int, int]]]] = ((
+        ("points", ((0, 0), (-10, -10))),
+        ("points", ((10, 10), (-9, -9))),
+        ("points", ((5, 5), (-6, 8))),
+        ("vector", ((-2, -1), (15, 5)))
+    ), (
+        ("points", ((9, 0), (-10, -10))),
+        ("points", ((19, 10), (-9, -9))),
+        ("points", ((9, 5), (-6, 8))),
+        ("vector", ((-2, -1), (15, 5)))
+    ), (
+        ("points", ((5, 0), (-10, -10))),
+        ("points", ((15, 10), (-9, -9))),
+        ("points", ((5, 5), (-6, 8))),
+        ("vector", ((-2, -1), (15, 5)))
+    ))
+
+    result = plot.plot(data, (-20, -20), (20, 20), 3)
+    print(result)
+
+def test_plot7():
+    importlib.reload(plot)
+
+    data: Iterable[Iterable[Iterable[Tuple[int, int]]]] = ((
+        ("points", ((0, 0), (-10, -10))),
+        ("points", ((10, 10), (-9, -9))),
+        ("points", ((5, 5), (-6, 8)))
+    ), (
+        ("points", ((9, 0), (-10, -10))),
+        ("points", ((19, 10), (-9, -9))),
+        ("points", ((9, 5), (-6, 8)))
+    ), (
+        ("points", ((5, 0), (-10, -10))),
+        ("points", ((15, 10), (-9, -9))),
+        ("points", ((5, 5), (-6, 8)))
+    ))
+    result = plot.plot(data, (-20, -20), (20, 20), 3)
+    print(result)
+
+def test_plot6():
+    importlib.reload(plot)
+
+    data: Iterable[Iterable[Iterable[Tuple[int, int]]]] = ((
+        ((0, 0), (-10, -10)),
+        ((10, 10), (-9, -9)),
+        ((5, 5), (-6, 8))
+    ),)
+    result = plot.scatter_animate(data, (-20, -20), (20, 20), 3)
+    print(result)
+
+def test_plot5():
+    importlib.reload(plot)
+
+    data: Iterable[Iterable[Iterable[Tuple[int, int]]]] = ((
+        ((0, 0), (-10, -10)),
+        ((10, 10), (-9, -9)),
+        ((5, 5), (-6, 8))
+    ), (
+        ((9, 0), (-10, -10)),
+        ((19, 10), (-9, -9)),
+        ((9, 5), (-6, 8))
+    ), (
+        ((5, 0), (-10, -10)),
+        ((15, 10), (-9, -9)),
+        ((5, 5), (-6, 8))
+    ))
+    result = plot.scatter_animate(data, (-20, -20), (20, 20), 3)
+    print(result)
+
+def test_plot4():
+    importlib.reload(plot)
+
+    data: Iterable[Iterable[Iterable[Tuple[int, int]]]] = ((
+        ((0, 0), (1, 1)),
+    ), (
+        ((5, 5), (10, 10)),
+    ), (
+        ((10, 10), (19, 19)),
+    ))
+    result = plot.scatter_animate(data, (-20, -20), (20, 20), 3)
+    print(result)
+
 def test_plot3():
     importlib.reload(plot)
 
